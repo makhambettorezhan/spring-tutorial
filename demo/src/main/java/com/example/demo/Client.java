a31c8f393de81c9810ee61df8d0e202eb466bdb6
Makhambet Torezhan <makhambet.torezhan@jits-3-26.jits.ipa.dc>
2022-05-04 15:55:21 +0600

    add: AOP

--- demo/src/main/java/com/example/demo/Client.java
+++ demo/src/main/java/com/example/demo/Client.java
@@ -1,13 +1,38 @@
 package com.example.demo;
 
+import java.util.List;
+
 public class Client {
     Service service;
+    private List someList;
+    private String name;
+    private Reminder reminder;
 
     Client(Service service) {
         this.service = service;
     }
-
+    public void setService(Service service) {
+        this.service = service;
+    }
     public void showMsg() {
         System.out.println(this.service.getMsg());
+    }
+
+    public void setSomeList(List someList) {
+        this.someList = someList;
+    }
+
+    public List getSomeList() {
+        return someList;
+    }
+
+    public Client(String name, Reminder reminder) {
+        this.name = name;
+        this.reminder = reminder;
+    }
+
+    public void action() {
+        this.reminder.increment();
+        System.out.printf("%s %d\n", this.name, this.reminder.getInt());
     }
 }
--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,14 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-		Client client = (Client) context.getBean("client");
-		client.showMsg();
+		//Client client = (Client) context.getBean("client");
+		//client.showMsg();
+		//System.out.println(client.getSomeList());
+
+		Client client1 = (Client) context.getBean("client1");
+		client1.action();
+		Client client2 = (Client) context.getBean("client2");
+		client2.action();
 	}
 
 }
